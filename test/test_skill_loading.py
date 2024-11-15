60888c3e168019e5e756c9117428f90beef85801
miro <jarbasai@mailfence.com>
2024-11-15 10:38:51 +0000

    fix: manifest and automations

--- test/test_skill_loading.py
+++ test/test_skill_loading.py
@@ -1,32 +1,21 @@
 import unittest
-from os.path import join, dirname
-import os
-from ovos_utils.bracket_expansion import expand_parentheses, expand_options
+from os.path import dirname
 
-from adapt.engine import IntentDeterminationEngine
-from adapt.intent import IntentBuilder
-from skill_ovos_fallback_unknown import UnknownSkill, create_skill
 from ovos_plugin_manager.skills import find_skill_plugins
 from ovos_utils.messagebus import FakeBus
-from mycroft.skills.skill_loader import PluginSkillLoader, SkillLoader
+from ovos_workshop.skill_launcher import PluginSkillLoader, SkillLoader
+from skill_ovos_fallback_unknown import UnknownSkill
 
 
 class TestSkillLoading(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         self.skill_id = "skill-ovos-fallback-unknown.openvoiceos"
-        self.path = dirname(dirname(dirname(__file__)))
+        self.path = dirname(dirname(__file__))
 
     def test_from_class(self):
         bus = FakeBus()
         skill = UnknownSkill()
-        skill._startup(bus, self.skill_id)
-        self.assertEqual(skill.bus, bus)
-        self.assertEqual(skill.skill_id, self.skill_id)
-
-    def test_from_func(self):
-        bus = FakeBus()
-        skill = create_skill()
         skill._startup(bus, self.skill_id)
         self.assertEqual(skill.bus, bus)
         self.assertEqual(skill.skill_id, self.skill_id)
@@ -63,5 +52,3 @@ class TestSkillLoading(unittest.TestCase):
         self.assertEqual(loader.skill_id, self.skill_id)
         self.assertEqual(loader.instance.bus, bus)
         self.assertEqual(loader.instance.skill_id, self.skill_id)
-
-
