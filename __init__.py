12d3d51f60ac714496a3001308e00cbf6247191a
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2025-06-09 21:09:36 +0100

    fix: missing import (#33)
    missed an import when adding typing

--- __init__.py
+++ __init__.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import List
+from ovos_bus_client.message import Message
 from ovos_workshop.skills.fallback import FallbackSkill
 from ovos_utils import classproperty
 from ovos_utils.process_utils import RuntimeRequirements
